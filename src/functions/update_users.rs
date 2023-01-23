778ab1a132d1ba18e91b974c9222dcc63cca705e
Ingo Reitz <ninelore@protonmail.com>
2023-01-23 06:45:06 +0100

    smol print change i guess

--- src/functions/update_users.rs
+++ src/functions/update_users.rs
@@ -2,9 +2,9 @@ use serenity::{prelude::Context, model::prelude::{GuildId, ChannelId}};
 
 pub async fn run(ctx: &Context, gid: GuildId) {
   let count = ctx.cache.guild(gid).unwrap().members.len();
-  if let Err(why) = ChannelId(1028632535418290257).edit(&ctx, |c| c.name(format!("Mitglieder: {:?}", count))).await {
+  if let Err(why) = ChannelId(1028632535418290257).edit(&ctx.http, |c| c.name(format!("Mitglieder: {:?}", count))).await {
     println!("Error while renaming: {:?}", why);
   } else {
-    println!("Das sollte geklappt haben...")
+    println!("Rename apparently successful")
   }
 }
