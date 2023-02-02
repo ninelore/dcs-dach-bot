a81ed164b60831741d70952fc07b8f14279ace70
Ingo Reitz <ninelore@protonmail.com>
2023-02-02 19:24:28 +0100

    add members_online

--- src/functions/update_users.rs
+++ src/functions/update_users.rs
@@ -1,10 +1,22 @@
+use serenity::model::user::OnlineStatus;
 use serenity::model::voice::VoiceState;
 use serenity::model::prelude::{GuildId, ChannelId, ChannelType, Channel};
 use serenity::prelude::Context;
 
 pub async fn members(ctx: &Context, gid: GuildId) {
   let count = ctx.cache.guild(gid).unwrap().members.len();
-  if let Err(why) = ChannelId(1067584919188029522).edit(&ctx.http, |c| c.name(format!("Mitglieder: {:?}", count))).await {
+  if let Err(why) = ChannelId(1028632535418290257).edit(&ctx.http, |c| c.name(format!("Mitglieder: {:?}", count))).await {
+    println!("Error while renaming: {:?}", why);
+  } else {
+    println!("Rename apparently successful");
+  }
+}
+
+pub async fn members_online(ctx: &Context, gid: GuildId) {
+  let count_offline = ctx.cache.guild(gid).unwrap().members_with_status(OnlineStatus::Offline).len();
+  let count_all = ctx.cache.guild(gid).unwrap().members.len();
+  let count = count_all - count_offline;
+  if let Err(why) = ChannelId(1067593250388594708).edit(&ctx.http, |c| c.name(format!("Mitglieder online: {:?}", count))).await {
     println!("Error while renaming: {:?}", why);
   } else {
     println!("Rename apparently successful");
@@ -21,7 +33,7 @@ pub async fn members_voice(ctx: &Context,  _old: Option<VoiceState>, _vs: VoiceS
       }
     }
   }
-  if let Err(why) = ChannelId(1067593373835350100).edit(&ctx.http, |c| c.name(format!("Mitglieder im Voice: {:?}", count))).await {
+  if let Err(why) = ChannelId(1029108215843127406).edit(&ctx.http, |c| c.name(format!("Mitglieder im Voice: {:?}", count))).await {
     println!("Error while renaming: {:?}", why);
   } else {
     println!("Rename apparently successful");
