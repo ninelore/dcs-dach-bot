347cd470d58b983a5163eb44aff6ee3722c36a92
Ingo Reitz <ninelore@protonmail.com>
2023-02-11 14:46:07 +0100

    finalize member count

--- src/functions/update_users.rs
+++ src/functions/update_users.rs
@@ -14,10 +14,11 @@ pub async fn members(ctx: &Context, gid: GuildId) {
 
 pub async fn members_online(ctx: &Context, gid: GuildId) {
   let cid = env::var("CHANNELID_ONLINE").expect("channelid_online").parse::<u64>().expect("channelid_online: u64");
-  let count_offline = ctx.cache.guild(gid).unwrap().members_with_status(OnlineStatus::Offline).len();
-  let count_all = ctx.cache.guild(gid).unwrap().members.len();
-  let count_on = count_all - count_offline;
-  if let Err(why) = ChannelId(cid).edit(&ctx.http, |c| c.name(format!("Mitglieder Online: {:?}", count_on))).await {
+  let guild = ctx.cache.guild(gid).unwrap();
+  let count2 = guild.members_with_status(OnlineStatus::Online).len() 
+    + guild.members_with_status(OnlineStatus::Idle).len()
+    + guild.members_with_status(OnlineStatus::DoNotDisturb).len();
+  if let Err(why) = ChannelId(cid).edit(&ctx.http, |c| c.name(format!("Mitglieder Online: {:?}", count2))).await {
     println!("Error while renaming: {:?}", why);
   } // */
 }
