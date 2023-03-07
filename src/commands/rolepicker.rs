e801be4ae02a49458b82b456745702c47d2f295c
Ingo Reitz <ninelore@protonmail.com>
2023-03-08 00:05:33 +0100

    finalize rolepicker?

--- src/commands/rolepicker.rs
+++ src/commands/rolepicker.rs
@@ -1,9 +1,7 @@
-use std::env;
-
 use indexmap::IndexMap;
-use serenity::all::{ResolvedValue, ComponentInteraction, CommandInteraction, GuildId, Role, ComponentInteractionDataKind};
+use serenity::all::{ResolvedValue, ComponentInteraction, CommandInteraction, Role, ComponentInteractionDataKind, RoleId};
 use serenity::{prelude::Context, model::Permissions};
-use serenity::builder::{CreateSelectMenuOption, CreateCommand, CreateCommandOption, CreateEmbed, CreateSelectMenu, CreateInteractionResponseMessage, CreateInteractionResponse, CreateSelectMenuKind};
+use serenity::builder::{CreateSelectMenuOption, CreateCommand, CreateCommandOption, CreateEmbed, CreateSelectMenu, CreateInteractionResponseMessage, CreateInteractionResponse, CreateSelectMenuKind, EditMessage};
 
 use crate::util::consts::*;
 
@@ -19,8 +17,8 @@ pub async fn create_picker(ctx: &Context, command: &CommandInteraction) {
 
 async fn role_picker(ctx: &Context, command: &CommandInteraction, name: String, roles: IndexMap<String, Vec<String>>) {
   let mut options: Vec<CreateSelectMenuOption> = Vec::new();
-  for (key, _val) in roles {
-    options.push(CreateSelectMenuOption::new(_val.first().unwrap(), &key));
+  for (key, val) in roles {
+    options.push(CreateSelectMenuOption::new(val.first().unwrap(), &key));
   }
 
   let _picker = command
@@ -30,40 +28,104 @@ async fn role_picker(ctx: &Context, command: &CommandInteraction, name: String, 
           .title(format!("Rollenwahl: {}", name))
           .field("Wähle deine Module", format!(" "), false)
         )
-      .select_menu(CreateSelectMenu::new("rolepicker".to_string(), CreateSelectMenuKind::String { options: options })
-        .max_values(10)
-      )
+      .select_menu(CreateSelectMenu::new("rolepicker".to_string(), CreateSelectMenuKind::String { options: options }))
     ))
     .await
-    .expect("something went wrong at module_picker");
+    .expect("Err while sending rolepicker");
 }
 
 pub async fn interaction(ctx: &Context, component: &ComponentInteraction) {
-  let guild_id = GuildId::new(
-    env::var("GUILD_ID")
-      .expect("Expected GUILD_ID in environment")
-      .parse()
-      .expect("GUILD_ID must be an integer"),
-  );
+  let gid = component.clone().guild_id.unwrap();
+  let guild = ctx.http.get_guild(gid).await.unwrap();
+  let mut user = guild.member(&ctx, component.user.id).await.unwrap();
 
-  let mut count = 0;
-  for (_key, val) in get_all() {
-    for role in &val {
-      let role_parsed: Role = {
-        let guild = ctx.cache.guild(guild_id).unwrap();
-        let role_parse = guild.role_by_name(role).expect("Err: Role not found").to_owned();
-        role_parse
-      };
-      if component.user.has_role(&ctx, guild_id, role_parsed).await.unwrap() {
-        count += 1;
+  let mut role_keys_cur: Vec<String> = Vec::new();
+  for (key, val) in get_all() {
+    let role_parsed: Role = {
+      let role = val.first().unwrap();
+      let guild = ctx.cache.guild(gid).unwrap();
+      if guild.role_by_name(role).is_none() { continue; }
+      let role_parse = guild.role_by_name(role).unwrap().to_owned();
+      role_parse
+    };
+    if component.clone().user.has_role(ctx, gid, role_parsed).await.unwrap() {
+      role_keys_cur.push(key);
+    }
+  }
+
+  let sel_roles = match component.clone().data.kind {
+    ComponentInteractionDataKind::StringSelect { values } => values,
+    _ => vec!["Error: unexpected interaction caught".to_string()],
+  };
+
+  let mut role_ids_add: Vec<RoleId> = Vec::new();
+  let mut role_ids_del: Vec<RoleId> = Vec::new();
+  let mut role_ids_cat_add: Vec<RoleId> = Vec::new();
+  let mut role_ids_cat_del: Vec<RoleId> = Vec::new();
+  for role in sel_roles.clone() {
+    let roleid_parsed: RoleId = {
+      if guild.role_by_name(get_all().get_key_value(&role).unwrap().1.first().unwrap()).is_none() { continue; }
+      let role_parse = guild.role_by_name(get_all().get_key_value(&role).unwrap().1.first().unwrap()).unwrap().to_owned();
+      role_parse.id
+    };
+    if role_keys_cur.contains(&role) {
+      role_ids_del.push(roleid_parsed);
+      for add_role in get_all().get_key_value(&role).unwrap().1.clone() {
+        if add_role.eq(&role) { continue; }
+        let roleid_parsed: RoleId = {
+          if guild.role_by_name(get_all().get_key_value(&add_role).unwrap().1.first().unwrap()).is_none() { continue; }
+          let role_parse = guild.role_by_name(get_all().get_key_value(&add_role).unwrap().1.first().unwrap()).unwrap().to_owned();
+          role_parse.id
+        };
+        if !user.roles.contains(&roleid_parsed) && !role_ids_cat_add.contains(&roleid_parsed) {
+          role_ids_cat_del.push(roleid_parsed)
+        }
+      }
+    } else {
+      role_ids_add.push(roleid_parsed);
+      for add_role in get_all().get_key_value(&role).unwrap().1.clone() {
+        if add_role.eq(&role) { continue; }
+        let roleid_parsed: RoleId = {
+          if guild.role_by_name(get_all().get_key_value(&add_role).unwrap().1.first().unwrap()).is_none() { continue; }
+          let role_parse = guild.role_by_name(get_all().get_key_value(&add_role).unwrap().1.first().unwrap()).unwrap().to_owned();
+          role_parse.id
+        };
+        if !user.roles.contains(&roleid_parsed) && !role_ids_cat_add.contains(&roleid_parsed) {
+          role_ids_cat_add.push(roleid_parsed)
+        }
       }
     }
   }
-  if count >= 10 {
-    let sel_roles = match component.clone().data.kind {
-      ComponentInteractionDataKind::StringSelect { values } => values,
-      _ => vec!["Error: unexpected interaction caught".to_string()],
-    };
+
+  for add_role in role_ids_cat_del.clone() {
+    if role_ids_cat_add.contains(&add_role) {
+      role_ids_cat_del.retain(|p| {p.ne(&add_role)});
+      role_ids_cat_add.retain(|p| {p.ne(&add_role)})
+    }
+  }
+
+  let len = role_keys_cur.len() + role_ids_add.len() - role_ids_del.len();
+  while len > 10 {
+    role_ids_add.pop();
+  }
+
+  let mut err = false;
+  if !role_ids_del.is_empty() {
+    if let Err(why) = user.remove_roles(ctx, role_ids_del.clone().as_slice()).await {
+      println!("Error while adding roles: {}", why);
+      err = true
+    } // */
+  }
+  if !role_ids_add.is_empty() {
+    if let Err(why) = user.add_roles(ctx, role_ids_add.clone().as_slice()).await {
+      println!("Error while adding roles: {}", why);
+      err = true
+    }
+  } // */
+  
+  if !err {
+    component.create_response(ctx, CreateInteractionResponse::Acknowledge).await.expect("Err while acknowledging ComponentInteraction");
+    component.clone().message.edit(ctx, EditMessage::new().content("")).await.expect("Err while editing rolepicker");
   }
 }
 
--- src/util/consts.rs
+++ src/util/consts.rs
@@ -4,23 +4,23 @@ use indexmap::IndexMap;
 pub fn get_jets() -> IndexMap<String, Vec<String>> {
 
   let mut models: IndexMap<String, Vec<String>> = IndexMap::new();
-  models.insert("a10c".to_string(), vec!["A-10C Warthog".to_string(), "Modern".to_string()]);
-  models.insert("ajs37".to_string(), vec!["AJS-37 Viggen".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("av8b".to_string(), vec!["AV-8B Night Attack V/STOL".to_string(), "Modern".to_string()]);
-  models.insert("c101".to_string(), vec!["C-101 Aviojet".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("f18c".to_string(), vec!["F/A-18C Hornet".to_string(), "Modern".to_string()]);
-  models.insert("f14p".to_string(), vec!["F-14 Tomcat Pilot".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("f14r".to_string(), vec!["F-14 Tomcat RIO".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("f16c".to_string(), vec!["F-16C Viper".to_string(), "Modern".to_string()]);
-  models.insert("f5".to_string(), vec!["F-5E Tiger II".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("f86".to_string(), vec!["F-86F Sabre".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("jf17".to_string(), vec!["JF-17 Thunder".to_string(), "Modern".to_string()]);
-  models.insert("l39".to_string(), vec!["L-39 Albatros".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("m2000".to_string(), vec!["M-2000C".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("mig15".to_string(), vec!["MiG-15bis".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("mig19".to_string(), vec!["MiG-19P Farmer".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("mig21".to_string(), vec!["MiG-21bis".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("f1".to_string(), vec!["Mirage F1".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("a10c".to_string(), vec!["Pilot A-10C Warthog".to_string(), "Modern".to_string()]);
+  models.insert("ajs37".to_string(), vec!["Pilot AJS-37 Viggen".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("av8b".to_string(), vec!["Pilot AV-8B Night Attack V/STOL".to_string(), "Modern".to_string()]);
+  models.insert("c101".to_string(), vec!["Pilot C-101 Aviojet".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("f18c".to_string(), vec!["Pilot F/A-18C Hornet".to_string(), "Modern".to_string()]);
+  models.insert("f14p".to_string(), vec!["Pilot F-14 Tomcat Pilot".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("f14r".to_string(), vec!["Pilot F-14 Tomcat RIO".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("f16c".to_string(), vec!["Pilot F-16C Viper".to_string(), "Modern".to_string()]);
+  models.insert("f5".to_string(), vec!["Pilot F-5E Tiger II".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("f86".to_string(), vec!["Pilot F-86F Sabre".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("jf17".to_string(), vec!["Pilot JF-17 Thunder".to_string(), "Modern".to_string()]);
+  models.insert("l39".to_string(), vec!["Pilot L-39 Albatros".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("m2000".to_string(), vec!["Pilot M-2000C".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("mig15".to_string(), vec!["Pilot MiG-15bis".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("mig19".to_string(), vec!["Pilot MiG-19P Farmer".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("mig21".to_string(), vec!["Pilot MiG-21bis".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("f1".to_string(), vec!["Pilot Mirage F1".to_string(), "Kalter Krieg".to_string()]);
 
   return models;
 
@@ -30,13 +30,13 @@ pub fn get_jets() -> IndexMap<String, Vec<String>> {
 pub fn get_fc() -> IndexMap<String, Vec<String>> {
 
   let mut models: IndexMap<String, Vec<String>> = IndexMap::new();
-  models.insert("a10a".to_string(), vec!["A-10A".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("f15c".to_string(), vec!["F-15C Eagle".to_string(), "Modern".to_string()]);
-  models.insert("mig29".to_string(), vec!["MiG-29".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("su25a".to_string(), vec!["Su-25A".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("su25t".to_string(), vec!["Su-25T".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("su27".to_string(), vec!["Su-27".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("su33".to_string(), vec!["Su-33".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("a10a".to_string(), vec!["Pilot A-10A".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("f15c".to_string(), vec!["Pilot F-15C Eagle".to_string(), "Modern".to_string()]);
+  models.insert("mig29".to_string(), vec!["Pilot MiG-29".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("su25a".to_string(), vec!["Pilot Su-25A".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("su25t".to_string(), vec!["Pilot Su-25T".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("su27".to_string(), vec!["Pilot Su-27".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("su33".to_string(), vec!["Pilot Su-33".to_string(), "Kalter Krieg".to_string()]);
 
   return models;
 
@@ -46,12 +46,12 @@ pub fn get_fc() -> IndexMap<String, Vec<String>> {
 pub fn get_helis() -> IndexMap<String, Vec<String>> {
 
   let mut models: IndexMap<String, Vec<String>> = IndexMap::new();
-  models.insert("ah64d".to_string(), vec!["AH-64D".to_string(), "Hubschrauber".to_string(), "Modern".to_string()]);
-  models.insert("ka50".to_string(), vec!["Black Shark".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("mi24".to_string(), vec!["Mi-24P Hind".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("mi8".to_string(), vec!["Mi-8MTV2 Magnificent Eight".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("uh1h".to_string(), vec!["UH-1H Huey".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("sa342".to_string(), vec!["SA342 Gazelle".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("ah64d".to_string(), vec!["Pilot AH-64D".to_string(), "Hubschrauber".to_string(), "Modern".to_string()]);
+  models.insert("ka50".to_string(), vec!["Pilot Black Shark".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("mi24".to_string(), vec!["Pilot Mi-24P Hind".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("mi8".to_string(), vec!["Pilot Mi-8MTV2 Magnificent Eight".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("uh1h".to_string(), vec!["Pilot UH-1H Huey".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("sa342".to_string(), vec!["Pilot SA342 Gazelle".to_string(), "Hubschrauber".to_string(), "Kalter Krieg".to_string()]);
 
   return models;
 
@@ -61,20 +61,29 @@ pub fn get_helis() -> IndexMap<String, Vec<String>> {
 pub fn get_props() -> IndexMap<String, Vec<String>> {
 
   let mut models: IndexMap<String, Vec<String>> = IndexMap::new();
-  models.insert("bf109".to_string(), vec!["Bf 109 K-4 Kurfürst".to_string(), "WWII".to_string()]);
-  models.insert("ce2".to_string(), vec!["Christen Eagle II".to_string(), "Modern".to_string()]);
-  models.insert("fw190a8".to_string(), vec!["Fw 190 A-8 Anton".to_string(), "WWII".to_string()]);
-  models.insert("fw190d9".to_string(), vec!["Fw 190 D-9 Dora".to_string(), "WWII".to_string()]);
-  models.insert("i16".to_string(), vec!["I-16".to_string(), "WWII".to_string()]);
-  models.insert("mosq".to_string(), vec!["Mosquito FB VI".to_string(), "WWII".to_string()]);
-  models.insert("p47".to_string(), vec!["P-47D Thunderbolt".to_string(), "WWII".to_string()]);
-  models.insert("p51".to_string(), vec!["P-51D Mustang".to_string(), "WWII".to_string()]);
-  models.insert("spitfire".to_string(), vec!["Spitfire LF Mk. IX".to_string(), "WWII".to_string()]);
-  models.insert("tf51".to_string(), vec!["TF-51D".to_string(), "Kalter Krieg".to_string()]);
-  models.insert("yak52".to_string(), vec!["Yak-52".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("bf109".to_string(), vec!["Pilot Bf 109 K-4 Kurfürst".to_string(), "WWII".to_string()]);
+  models.insert("ce2".to_string(), vec!["Pilot Christen Eagle II".to_string(), "Modern".to_string()]);
+  models.insert("fw190a8".to_string(), vec!["Pilot Fw 190 A-8 Anton".to_string(), "WWII".to_string()]);
+  models.insert("fw190d9".to_string(), vec!["Pilot Fw 190 D-9 Dora".to_string(), "WWII".to_string()]);
+  models.insert("i16".to_string(), vec!["Pilot I-16".to_string(), "WWII".to_string()]);
+  models.insert("mosq".to_string(), vec!["Pilot Mosquito FB VI".to_string(), "WWII".to_string()]);
+  models.insert("p47".to_string(), vec!["Pilot P-47D Thunderbolt".to_string(), "WWII".to_string()]);
+  models.insert("p51".to_string(), vec!["Pilot P-51D Mustang".to_string(), "WWII".to_string()]);
+  models.insert("spitfire".to_string(), vec!["Pilot Spitfire LF Mk. IX".to_string(), "WWII".to_string()]);
+  models.insert("tf51".to_string(), vec!["Pilot TF-51D".to_string(), "Kalter Krieg".to_string()]);
+  models.insert("yak52".to_string(), vec!["Pilot Yak-52".to_string(), "Kalter Krieg".to_string()]);
 
   return models;
 
+}
+
+#[allow(unused)]
+pub fn get_other() -> IndexMap<String, Vec<String>> {
+
+  let mut models: IndexMap<String, Vec<String>> = IndexMap::new();
+  //
+
+  return models;
 }
 
 #[allow(unused)]
